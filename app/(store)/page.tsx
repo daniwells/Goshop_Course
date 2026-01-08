e6e6b7894948dff1ce42db170445a594ccccd650
daniwells <daniellimafreita@gmail.com>
2026-01-08 15:26:05 -0300

    feat: Developing cart store state with zustand

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -2,10 +2,14 @@
 // import { seedCountries } from "@/migration-scripts/seed-countries";
 import { getProducts } from "@/queries/product";
 import ProductList from "@/components/store/shared/product-list";
+import useFromStore from "@/hooks/useFromStore";
+import { useCartStore } from "@/cart-store/userCartStore";
 
 export default async function HomePage() {
   const productsData = await getProducts();
   const { products } = productsData;
+  const cart = useFromStore(useCartStore, (state) => state.cart);
+  const addTocart = useCartStore((state) => state.addToCart);
 
   return (
     <div className="p-14">
---
+++ cart-store/userCartStore.ts
@@ -0,0 +1,124 @@
+import { CartProductType } from "@/lib/types"
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+// Define the interface of the Cart state
+interface State {
+    cart: CartProductType[]
+    totalItems: number
+    totalPrice: number
+}
+
+// Define the interface of the actions that can be performed in the Cart
+interface Actions {
+    addToCart: (Item: CartProductType) => void;
+    updateProductQuantity: (product: CartProductType, quantity: number) => void;
+    removeMultipleFromCart: (items: CartProductType[]) => void;
+    removeFromCart: (Item: CartProductType) => void;
+    emptyCart: () => void;
+};
+
+// Initialize a default state
+const INITIAL_STATE: State = {
+    cart: [],
+    totalItems: 0,
+    totalPrice: 0,
+};
+
+// Create the store with Zustand, combining the status interface and actions
+export const useCartStore = create(
+    persist<State & Actions>((set, get) => ({
+        cart: INITIAL_STATE.cart,
+        totalItems: INITIAL_STATE.totalItems,
+        totalPrice: INITIAL_STATE.totalPrice,
+        addToCart: (product: CartProductType) => {
+            if(!product) return;
+            const cart = get().cart;
+            const cartItem = cart.find(
+                (item) => item.productId === product.productId &&
+                item.variantId === product.variantId &&
+                item.sizeId === product.sizeId
+            );
+
+            if(cartItem){
+                const updatedCart = cart.map((item) => 
+                    item.productId === product.productId &&
+                    item.variantId === product.variantId &&
+                    item.sizeId === product.sizeId
+                        ? {...item, quantity: item.quantity+product.quantity}
+                        : item
+                );
+                set((state) => ({
+                    cart: updatedCart,
+                    totalPrice: state.totalPrice + product.price * product.quantity,
+                }));
+            }else{
+                const updatedCart = [...cart, { ...product }];
+                set((state) => ({
+                    cart: updatedCart,
+                    totalItems: state.totalItems + 1,
+                    totalPrice: state.totalPrice + product.price * product.quantity,
+                }));
+            }
+        },
+        updateProductQuantity: (product: CartProductType, quantity: number) => {
+            const cart = get().cart;
+
+            if(quantity <= 0){
+                get().removeFromCart(product);
+                return;
+            }
+
+            const updatedCart = cart.map((item) => 
+                item.productId === product.productId &&
+                item.variantId === product.variantId &&
+                item.sizeId === product.sizeId
+                ? { ...item, quantity }
+                : item
+            );
+
+            const totalItems = updatedCart.length;
+            const totalPrice = updatedCart.reduce(
+                (sum, item) => sum+item.price * item.quantity, 0
+            );
+            set(() => ({
+                cart: updatedCart,
+                totalItems,
+                totalPrice,
+            }));
+        },
+        removeFromCart: (product: CartProductType) => {
+            const cart = get().cart;
+            const updatedCart = cart.filter((item) => !(
+                item.productId === product.productId &&
+                item.variantId === product.variantId &&
+                item.sizeId === product.sizeId
+            ));
+            const totalItems = updatedCart.length;
+            const totalPrice = updatedCart.reduce(
+                (sum, item) => sum+item.price * item.quantity, 0
+            );
+            set(() => ({
+                cart: updatedCart,
+                totalItems,
+                totalPrice,
+            }));
+        },
+        removeMultipleFromCart: (products: CartProductType[]) => {
+            products.forEach((product) => {
+                get().removeFromCart(product);
+            })
+        },
+        emptyCart: () => {
+            set(() => ({
+                cart: [],
+                totalItems: 0,
+                totalPrice: 0,
+            }))
+        },
+    }),
+    {
+        name: "cart",
+    })
+);
+
